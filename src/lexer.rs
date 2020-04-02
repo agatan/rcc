dc6c0dce19281e3fe2501a9153d081c2ce0b96bd
agatan <agatan039@gmail.com>
2020-04-02 17:48:29 +0900

    Refactor

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,39 +1,74 @@
-macro_rules! error {
-    ($($arg:tt)*) => {
-        {
-            eprintln!($($arg)*);
-            std::process::exit(1)
+#[cfg_attr(test, derive(Debug, PartialEq))]
+#[derive(Clone, Copy)]
+pub enum Location {
+    At(usize),
+    Span(usize, usize),
+}
+
+#[cfg_attr(test, derive(Debug, PartialEq))]
+enum LexErrorKind {
+    UnexpectedCharacter(char),
+}
+
+#[cfg_attr(test, derive(Debug, PartialEq))]
+pub struct LexError<'a> {
+    kind: LexErrorKind,
+    location: Location,
+    source: &'a str,
+}
+
+impl<'a> LexError<'a> {
+    pub fn location(&self) -> Location {
+        self.location
+    }
+
+    pub fn source(&self) -> &'a str {
+        self.source
+    }
+}
+
+impl<'a> std::fmt::Display for LexError<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        use LexErrorKind::*;
+        match self.kind {
+            UnexpectedCharacter(c) => write!(f, "unexpected character: {:?}", c),
         }
-    };
+    }
 }
 
 pub enum Token<'a> {
     Reserved(&'a str),
     Num(i32),
-    Eof,
 }
 
 impl<'a> std::fmt::Display for Token<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Token::Reserved(s) => write!(f, "{}", s),
+            Token::Reserved(s) => write!(f, "keyword {:?}", s),
             Token::Num(i) => write!(f, "{}", i),
-            Token::Eof => write!(f, "EOF"),
         }
     }
 }
 
-pub struct Tokenizer<'a> {
+pub type Lexed<'a> = Option<(Token<'a>, Location)>;
+pub type Result<'a, T> = std::result::Result<T, LexError<'a>>;
+pub type LexResult<'a> = Result<'a, Lexed<'a>>;
+
+pub struct Lexer<'a> {
     char_indices: std::iter::Peekable<std::str::CharIndices<'a>>,
     source: &'a str,
 }
 
-impl<'a> Tokenizer<'a> {
+impl<'a> Lexer<'a> {
     pub fn new(input: &'a str) -> Self {
-        Tokenizer {
+        Lexer {
             char_indices: input.char_indices().peekable(),
             source: input,
         }
+    }
+
+    pub fn source(&self) -> &'a str {
+        self.source
     }
 
     fn offset(&mut self) -> usize {
@@ -70,33 +105,44 @@ impl<'a> Tokenizer<'a> {
         })
     }
 
-    fn lex_number(&mut self) -> Option<Token<'a>> {
+    fn lex_number(&mut self) -> Lexed<'a> {
         let start = self.offset();
         while self.eat_if(|c| c.is_digit(10)) {}
         let end = self.offset();
 
+        if start == end {
+            return None;
+        }
+
         let v = self.source[start..end]
             .parse()
             .expect("expected valid i32 value");
-        Some(Token::Num(v))
+        Some((Token::Num(v), Location::Span(start, end)))
     }
 
-    pub fn tokenize(&mut self) -> Token<'a> {
+    pub fn lex(&mut self) -> LexResult<'a> {
         while self.eat_whitespace() {}
 
-        let (offset, ch) = match self.char_indices.peek() {
-            None => return Token::Eof,
-            Some(x) => *x,
-        };
+        let offset = self.offset();
 
         if self.eat_char('+') || self.eat_char('-') {
-            return Token::Reserved(&self.source[offset..offset + 1]);
+            return Ok(Some((
+                Token::Reserved(&self.source[offset..offset + 1]),
+                Location::Span(offset, offset + 1),
+            )));
         }
 
         if let Some(token) = self.lex_number() {
-            return token;
+            return Ok(Some(token));
         }
 
-        error!("unepxcted charactor: {}", ch);
+        match self.char_indices.peek() {
+            None => Ok(None),
+            Some(&(offset, ch)) => Err(LexError {
+                kind: LexErrorKind::UnexpectedCharacter(ch),
+                source: self.source,
+                location: Location::At(offset),
+            }),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,163 @@
 use std::env;
 
 mod lexer;
-use lexer::{Tokenizer, Token};
+use lexer::{Lexer, Location, Token};
 
-macro_rules! error {
-    ($($arg:tt)*) => {
-        {
-            eprintln!($($arg)*);
-            std::process::exit(1)
-        }
+pub fn describe_location(
+    f: &mut std::fmt::Formatter,
+    source: &str,
+    location: Location,
+) -> std::fmt::Result {
+    let start = match location {
+        Location::At(s) => s,
+        Location::Span(s, _) => s,
     };
-}
-
-fn expect_number<'a>(tokenizer: &mut Tokenizer<'a>) -> i32 {
-    match tokenizer.tokenize() {
-        Token::Num(v) => v,
-        t => error!("unexpected token: {}", t),
+    if source.len() <= start {
+        write!(f, " caused at end of input")
+    } else {
+        let line_number = source[..start].lines().count();
+        let current_line_start = source[..start].rfind('\n').unwrap_or(0);
+        let current_line_end = source[start..]
+            .find(|c| c == '\r' || c == '\n')
+            .map(|i| i + start)
+            .unwrap_or(source.len());
+        let current_line = &source[current_line_start..current_line_end];
+        let column = start - current_line_start + 1;
+        writeln!(f, " caused at {}:{}:", line_number, column)?;
+        writeln!(f, "  | {}", current_line)?;
+        match location {
+            Location::At(_) => writeln!(f, "  . {:>width$}", "^", width = column),
+            Location::Span(_, end) => writeln!(
+                f,
+                "  . {:>width$}{:~>trailing$}",
+                "^",
+                "",
+                width = column,
+                trailing = std::cmp::min(current_line_end, end) - column
+            ),
+        }
     }
 }
 
-fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() != 2 {
-        error!("{}: invalid number of arguments", args[0])
+enum ErrorKind<'a> {
+    LexError(lexer::LexError<'a>),
+    UnexpectedToken {
+        got: Token<'a>,
+        expected: Option<&'static str>,
+    },
+    UnexpectedEndOfFile {
+        expected: Option<&'static str>,
+    },
+}
+
+struct Error<'a> {
+    kind: ErrorKind<'a>,
+    location: Location,
+    source: &'a str,
+}
+
+impl<'a> std::fmt::Display for Error<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        use ErrorKind::*;
+        match &self.kind {
+            LexError(e) => e.fmt(f)?,
+            UnexpectedToken { got, expected } => match expected {
+                Some(expected) => write!(f, "expected {} but got {}", expected, got)?,
+                None => write!(f, "unexpected token {}", got)?,
+            },
+            UnexpectedEndOfFile { expected } => match expected {
+                Some(expected) => write!(f, "expected {} but reached EOF", expected)?,
+                None => write!(f, "reached EOF")?,
+            },
+        }
+        describe_location(f, self.source, self.location)
     }
-    let mut tokenizer = Tokenizer::new(&args[1]);
+}
+
+impl<'a> std::convert::From<lexer::LexError<'a>> for Error<'a> {
+    fn from(v: lexer::LexError<'a>) -> Self {
+        Self {
+            location: v.location(),
+            source: v.source(),
+            kind: ErrorKind::LexError(v),
+        }
+    }
+}
+
+fn expect_number<'a>(lexer: &mut Lexer<'a>) -> Result<i32, Error<'a>> {
+    match lexer.lex()? {
+        Some((Token::Num(v), _)) => Ok(v),
+        Some((t, loc)) => Err(Error {
+            kind: ErrorKind::UnexpectedToken {
+                got: t,
+                expected: Some("number"),
+            },
+            location: loc,
+            source: lexer.source(),
+        }),
+        None => Err(Error {
+            kind: ErrorKind::UnexpectedEndOfFile {
+                expected: Some("number"),
+            },
+            location: Location::At(lexer.source().len()),
+            source: lexer.source(),
+        }),
+    }
+}
+
+fn gen(source: &str) -> Result<(), Error> {
+    let mut lexer = Lexer::new(source);
 
     println!(".intel_syntax noprefix");
     println!(".global _main");
     println!("_main:");
 
-    let v = match tokenizer.tokenize() {
-        Token::Num(v) => v,
-        t => error!("unexpected token: {}", t),
-    };
+    let v = expect_number(&mut lexer)?;
     println!("  mov rax, {}", v);
 
     loop {
-        let token = tokenizer.tokenize();
+        let token = lexer.lex().map_err(|err| Error::from(err))?;
+        let (token, location) = match token {
+            None => break,
+            Some((token, location)) => (token, location),
+        };
         match token {
-            Token::Eof => break,
             Token::Reserved("+") => {
-                println!("  add rax, {}", expect_number(&mut tokenizer));
+                let rhs = expect_number(&mut lexer)?;
+                println!("  add rax, {}", rhs);
             }
             Token::Reserved("-") => {
-                println!("  sub rax, {}", expect_number(&mut tokenizer));
+                let rhs = expect_number(&mut lexer)?;
+                println!("  sub rax, {}", rhs);
             }
-            t => error!("unexpected token: {}", t),
+            t => {
+                return Err(Error {
+                    kind: ErrorKind::UnexpectedToken {
+                        got: t,
+                        expected: None,
+                    },
+                    location: location,
+                    source: lexer.source(),
+                })
+            }
         }
     }
 
     println!("  ret");
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("{}: invalid number of arguments", args[0]);
+        std::process::exit(1);
+    }
+    match gen(&args[1]) {
+        Ok(()) => {}
+        Err(err) => {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        }
+    }
 }
